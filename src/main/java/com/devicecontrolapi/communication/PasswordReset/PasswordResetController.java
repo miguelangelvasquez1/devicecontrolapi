f21d202532f695c13e9a7be09d471e651495f2a3
Miguel Vasquez <vgmiguel16@gmail.com>
2025-06-12 00:50:04 -0500

    Password reset step 1 modification

--- src/main/java/com/devicecontrolapi/communication/PasswordReset/PasswordResetController.java
+++ src/main/java/com/devicecontrolapi/communication/PasswordReset/PasswordResetController.java
@@ -4,6 +4,7 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,7 +14,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devicecontrolapi.communication.PasswordResetSender;
-import com.devicecontrolapi.dto.EmailPasswordResetRequest;
 import com.devicecontrolapi.dto.PasswordResetRequest;
 import com.devicecontrolapi.model.Usuario;
 import com.devicecontrolapi.repository.UsuarioRepository;
@@ -36,24 +36,30 @@ public class PasswordResetController {
     @Autowired
     private PasswordResetSender passwordResetSender; // Llama a tu Azure Function
 
-    @PostMapping("/forgot-password") //Enviar email para restablecer contraseña
-    public ResponseEntity<?> forgotPassword(@RequestBody EmailPasswordResetRequest request) {
-        String email = request.getEmail();
-        String username = request.getName();
+    @PostMapping("/forgot-password") // Enviar email para restablecer contraseña
+    public ResponseEntity<?> forgotPassword(@RequestBody String email) {
 
-        if (!usuarioRepository.existsByEmail(email)) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Correo no registrado");
+        Usuario user = usuarioRepository.findByEmail(email);
+
+        if (user == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .body(Map.of("error", "Correo no registrado"));
         }
+
+        String username = user.getNombre();
 
         String code = passwordResetService.generateResetCode(email);
 
         // Enviar correo usando Azure Function
-        passwordResetSender.sendResetCode(email, username, code); //Esto puede no enviarse, mostrar mensaje en el front
+        passwordResetSender.sendResetCode(email, username, code); // Esto puede no enviarse, mostrar mensaje en el front
 
-        return ResponseEntity.ok("Código de verificación enviado al correo.");
+        return ResponseEntity.ok()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(Map.of("message", "Código de verificación enviado al correo."));
     }
 
-    @PostMapping("/verify-code") //Verificar si el código es correcto
+    @PostMapping("/verify-code") // Verificar si el código es correcto
     public ResponseEntity<?> verifyCode(@RequestBody Map<String, String> request) {
         String email = request.get("email");
         String code = request.get("code");
@@ -62,22 +68,22 @@ public class PasswordResetController {
         if (!valid)
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Código inválido o expirado");
 
-        //Eliminar token?
+        // Eliminar token?
         return ResponseEntity.ok("Código válido, procede a restablecer tu contraseña.");
     }
 
-    @Transactional //Por el delete del token
-    @PostMapping("/reset-password") //Para cambiar la contraseña
+    @Transactional // Por el delete del token
+    @PostMapping("/reset-password") // Para cambiar la contraseña
     public ResponseEntity<?> resetPassword(@RequestBody PasswordResetRequest request) {
         if (!passwordResetService.validateResetCode(request.getEmail(), request.getCode())) {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Código inválido o expirado");
-        } //Verifica de nuevo el código
+        } // Verifica de nuevo el código
 
         Usuario user = usuarioRepository.findByEmail(request.getEmail());
-                // .orElseThrow(() -> new RuntimeException("Usuario no encontrado"));
+        // .orElseThrow(() -> new RuntimeException("Usuario no encontrado"));
 
         user.setClave(passwordEncoder.encode(request.getNewPassword()));
-        usuarioRepository.save(user); //Sobreescribe el usuario con la nueva contraseña
+        usuarioRepository.save(user); // Sobreescribe el usuario con la nueva contraseña
 
         tokenRepository.deleteByEmail(request.getEmail());
 
